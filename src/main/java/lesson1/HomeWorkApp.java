56e50b2137777a5747e097ffd5820dbd641269b7
vadimsibQa <vadimsib.qa@gmail.com>
2024-01-27 10:23:13 +0300

    Completed HomeWork #1

---
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,65 @@
+package lesson1;
+
+//1. Создать удаленный репозиторий на GitHub, подключить его к IDE в качестве проекта, добавить к нему модуль Maven.
+// Создать новую ветку в репозитории в IntelliJ IDEA, создать класс HomeWorkApp, и прописать в нем метод main().
+public class HomeWorkApp {
+//    2. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+//    3. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их
+//    любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные, и если их сумма больше
+//    или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
+    public static void checkSumSign() {
+        int a = -10;
+        int b = 5;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+//    4. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым
+//    значением. Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”, если
+//    лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, если больше 100 (100
+//    исключительно) - “Зеленый”;
+    public static void printColor() {
+        int value = 101;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else
+            System.out.println("Зеленый");
+    }
+
+//    5. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и инициализируйте их
+//    любыми значениями, которыми захотите. Если a больше или равно b, то необходимо вывести в консоль сообщение “a
+//    >= b”, в противном случае “a < b”;
+    public static void compareNumbers() {
+        int a = 1;
+        int b = 10;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+
+//    6. Методы из пунктов 2-5 вызовите из метода main() и посмотрите, корректно ли они работают.
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+}
--- src/main/java/lesson1/MyFirstClass.java
+++
@@ -1,7 +0,0 @@
-package lesson1;
-
-public class MyFirstClass {
-    public static void main(String[] args) {
-        System.out.println("Test 2!");
-    }
-}
